56b4f4d5a97153a591aeee4dbb8aa79a922e6d19
s2s2hyun <s2s2hyun0601@gmail.com>
2024-11-15 17:28:45 +0900

    background change

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center bg-black">
+          <main className="min-h-screen flex flex-col items-center bg-[#191919]">
             {/* <div className="flex-1 w-full flex flex-col gap-20 items-center"> */}
               <div className="flex flex-col gap-20 max-w-5xl p-5">
                 {children}
