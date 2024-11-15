7994ee13b2820c2aaea2294a1e9d41136daf0246
s2s2hyun <s2s2hyun0601@gmail.com>
2024-11-15 16:28:37 +0900

    cors post

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" }, // 모든 도메인 허용
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
+            { key: "Access-Control-Allow-Methods", value: "GET,POST,OPTIONS,PUT,DELETE" },
             { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Content-Type, Authorization" },
           ],
         },
