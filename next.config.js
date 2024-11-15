71a9f13f1a9dd984cfffd495234dce0ade7918d4
s2s2hyun <s2s2hyun0601@gmail.com>
2024-11-15 13:21:14 +0900

    cors error

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-module.exports = nextConfig;
+const nextConfig = {
+    async headers() {
+      return [
+        {
+          source: "/api/:path*", // 모든 API 경로에 대해 CORS 헤더 설정
+          headers: [
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" }, // 모든 도메인 허용
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS" },
+            { key: "Access-Control-Allow-Headers", value: "X-Requested-With, Content-Type, Authorization" },
+          ],
+        },
+      ];
+    },
+  };
+  
+  module.exports = nextConfig;
+  
