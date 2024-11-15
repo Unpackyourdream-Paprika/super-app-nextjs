d5e9309191d60df6b36daff4a8b0a3babb650912
s2s2hyun <s2s2hyun0601@gmail.com>
2024-11-15 17:54:32 +0900

    w-full version

--- app/comment/page.tsx
+++ app/comment/page.tsx
@@ -370,7 +370,7 @@ export default function Comments() {
   };
 
   return (
-    <div className="max-w-2xl mx-auto p-4">
+    <div className="w-full mx-auto p-4">
       <CommentForm />
       <CommentList />
     </div>
